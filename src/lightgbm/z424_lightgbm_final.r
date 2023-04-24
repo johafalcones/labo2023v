cbc495d59a34ced8f19d7949a5332f5cae683ee7
johafalcones <jefalconesgarcia@mail.austral.edu.ar>
2023-04-24 08:05:47 -0400

    updated files
    updated files

--- src/lightgbm/z424_lightgbm_final.r
+++ src/lightgbm/z424_lightgbm_final.r
@@ -20,13 +20,13 @@ PARAM$input$dataset       <- "./datasets/dataset_pequeno.csv"
 PARAM$input$training      <- c( 202107 )   #meses donde se entrena el modelo
 PARAM$input$future        <- c( 202109 )   #meses donde se aplica el modelo
 
-PARAM$finalmodel$semilla           <- 102191
+PARAM$finalmodel$semilla           <- 100049
 
-PARAM$finalmodel$num_iterations    <-  559
-PARAM$finalmodel$learning_rate     <-    0.0100746999
-PARAM$finalmodel$feature_fraction  <-    0.5144127527
-PARAM$finalmodel$min_data_in_leaf  <-    505
-PARAM$finalmodel$num_leaves        <-     44
+PARAM$finalmodel$num_iterations    <-  543
+PARAM$finalmodel$learning_rate     <-    0.010082684
+PARAM$finalmodel$feature_fraction  <-    0.397028365
+PARAM$finalmodel$min_data_in_leaf  <-    668
+PARAM$finalmodel$num_leaves        <-     131
 
 
 PARAM$finalmodel$max_bin           <-     31
