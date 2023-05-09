3b860d536f84868209a65c6323a1a26c3a87e930
johafalcones <jefalconesgarcia@mail.austral.edu.ar>
2023-05-08 23:33:14 -0400

    No underfitting

--- src/colaborativos-default/NoUnderfitting/641_TS_training_strategy.r
+++ src/colaborativos-default/NoUnderfitting/641_TS_training_strategy.r
@@ -20,19 +20,20 @@ PARAM$experimento <- "TS6410"
 
 PARAM$exp_input  <- "FE6310"
 
-# entreno en 6 meses
+# me salteo los meses duros de pandemia, pero llego hasta 201907 en training
+# entreno en 18 meses
 
 PARAM$future       <- c( 202109 )
-PARAM$final_train  <- c( 202107, 202106, 202105, 202104, 202103, 202102)
+PARAM$final_train  <- c( 202107, 202106, 202105, 202104, 202103, 202102, 202101, 202012, 202011, 202010, 202009, 202008, 202002, 202001, 201912, 201911, 201910, 201909 )
 
-PARAM$train$training     <- c( 202105, 202104, 202103, 202102, 202101, 202012)
+PARAM$train$training     <- c( 202105, 202104, 202103, 202102, 202101, 202012, 202011, 202010, 202009, 202008, 202002, 202001, 201912, 201911, 201910, 201909, 201908, 201907 )
 PARAM$train$validation   <- c( 202106 )
 PARAM$train$testing      <- c( 202107 )
 
 # Atencion  0.4  de  undersampling de la clase mayoritaria,  los CONTINUA
-PARAM$train$undersampling  <- 0.4   # 1.0 significa NO undersampling ,  0.1  es quedarse con el 10% de los CONTINUA
+PARAM$train$undersampling  <- 1   # primera corrida sin undersampling (1.0 significa NO undersampling ,  0.1  es quedarse con el 10% de los CONTINUA)
 
-PARAM$train$semilla  <- 100049  #primera semila (cambiar por su propia semilla  !!!) 
+PARAM$train$semilla  <- 100049  # cambiar por su propia semilla  !!! 
 # FIN Parametros del script
 
 
