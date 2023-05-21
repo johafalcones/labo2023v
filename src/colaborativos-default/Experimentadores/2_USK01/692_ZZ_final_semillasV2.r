84e3754045d2bfcf9d6b38a7f342f3d0c05c462c
johafalcones <jefalconesgarcia@mail.austral.edu.ar>
2023-05-21 18:40:24 -0400

    cambiando semillas 01

--- src/colaborativos-default/Experimentadores/2_USK01/692_ZZ_final_semillasV2.r
+++ src/colaborativos-default/Experimentadores/2_USK01/692_ZZ_final_semillasV2.r
@@ -36,7 +36,7 @@ PARAM$exp_input  <- "HT6510_US01"
 
 #Atencion, que se procesan 5 modelos y cada uno con 5 semillas, ajuste a SUS necesidades
 PARAM$modelos_rank  <- c( 1, 2, 5, 10, 20, 50 )  #Que modelos quiero, segun su posicion en el ranking
-PARAM$semillas  <- c(100049,101771,102061,113123,556861) #reemplazar por las propias semillas
+PARAM$semillas  <- c(200689, 200707, 200909, 201511, 201997) #reemplazar por las propias semillas
 
 PARAM$kaggle$envios_desde  <-  8000
 PARAM$kaggle$envios_hasta  <- 13500
